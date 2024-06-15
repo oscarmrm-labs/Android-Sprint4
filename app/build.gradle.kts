1d8183bcf2936b67995b8a9e0bd001b4d3699353
OMRM <oscarmrm14@gmail.com>
2024-06-15 11:59:13 +0200

    add navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Navegacion
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 }
