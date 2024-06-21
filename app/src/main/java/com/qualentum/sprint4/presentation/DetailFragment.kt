91c7a25e598b97a87a623ac23fd8c6a0c6270b10
OMRM <oscarmrm14@gmail.com>
2024-06-21 13:04:10 +0200

    Send args through fragments

--- app/src/main/java/com/qualentum/sprint4/presentation/DetailFragment.kt
+++ app/src/main/java/com/qualentum/sprint4/presentation/DetailFragment.kt
@@ -5,26 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.qualentum.sprint4.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private lateinit var binding: FragmentDetailBinding
+
+    val args: DetailFragmentArgs by navArgs()
+    private lateinit var name: String
+    private var age: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            //param1 = it.getString(ARG_PARAM1)
-            //param2 = it.getString(ARG_PARAM2)
-        }
+        getContactArgs()
+    }
+
+    private fun getContactArgs() {
+        name = args.name
+        age = args.age
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
+        bindContactDetail()
         return binding.root
+    }
+
+    private fun bindContactDetail() {
+        binding.tvName.text = name
+        binding.tvAge.text = age.toString()
     }
 }
